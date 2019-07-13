ceabb4397892e8e16138d51f9dff993a3b8c1ef8
Az <xuchenzhe0211@gmail.com>
2019-07-13 14:16:07 -0700

    Create Quick Sort.java

---
+++ Training Plan - Algorithm Class/Quick Sort.java
@@ -0,0 +1,46 @@
+/*
+Given an array of integers, sort the elements in the array in ascending order. The quick sort algorithm should be used to solve this problem.
+
+Examples
+
+{1} is sorted to {1}
+{1, 2, 3} is sorted to {1, 2, 3}
+{3, 2, 1} is sorted to {1, 2, 3}
+{4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+
+Corner Cases
+
+What if the given array is null? In this case, we do not need to do anything.
+What if the given array is of length zero? In this case, we do not need to do anything.
+*/
+
+public class Solution {
+  public int[] quickSort(int[] array) {
+    if (array == null || array.length == 0) return array;
+    quickSort(array, 0, array.length - 1);
+    return array;
+  }
+  private void quickSort(int[] array, int left, int right) {
+    if (left >= right) return;
+    int pivotIndex = left + new Random().nextInt(right - left + 1);
+    int pivotValue = array[pivotIndex];
+    swap(array, pivotIndex, right);
+    int idxLeft = left;
+    int idxRight = right - 1;
+    while (idxLeft <= idxRight) {
+      if (array[idxLeft] <= pivotValue) {
+        idxLeft++;
+      } else {
+        swap(array, idxLeft, idxRight--);
+      }
+    }
+    swap(array, right, idxLeft);
+    quickSort(array, left, idxLeft - 1);
+    quickSort(array, idxLeft + 1, right);
+  }
+  private void swap(int[] array, int i1, int i2) {
+    int temp = array[i1];
+    array[i1] = array[i2];
+    array[i2] = temp;
+  }
+}
