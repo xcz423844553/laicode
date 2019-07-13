c017fa932c4c47c38f0adbcc55acd104efbfca0d
Az <xuchenzhe0211@gmail.com>
2019-07-13 15:18:17 -0700

    Create Reverse LinkedList.java

---
+++ Training Plan - Algorithm Class/Reverse LinkedList.java
@@ -0,0 +1,33 @@
+/*
+Reverse a singly-linked list.
+
+Examples
+
+L = null, return null
+L = 1 -> null, return 1 -> null
+L = 1 -> 2 -> 3 -> null, return 3 -> 2 -> 1 -> null
+*/
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode reverse(ListNode head) {
+    ListNode prev = null;
+    ListNode cur = head;
+    while (cur != null) {
+      ListNode next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+    }
+    return prev;
+  }
+}
