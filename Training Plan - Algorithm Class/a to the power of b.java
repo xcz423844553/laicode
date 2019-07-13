8d3ae11d3b972d2d2ef3b80bf636bba0dc4fb86f
Az <xuchenzhe0211@gmail.com>
2019-07-13 13:47:35 -0700

    Create a to the power of b.java

---
+++ Training Plan - Algorithm Class/a to the power of b.java
@@ -0,0 +1,23 @@
+/*
+Evaluate a to the power of b, assuming both a and b are integers and b is non-negative.
+
+Examples
+
+power(2, 0) = 1
+power(2, 3) = 8
+power(0, 10) = 0
+power(-2, 5) = -32
+
+Corner Cases
+
+In this question, we assume 0^0 = 1.
+What if the result is overflowed? We can assume the result will not be overflowed when we solve this problem on this online judge.
+*/
+
+public class Solution {
+  public long power(int a, int b) {
+    if (b == 0) return 1;
+    long half = power(a, b / 2);
+    return b % 2 == 0 ? half * half : half * half * a;
+  }
+}
