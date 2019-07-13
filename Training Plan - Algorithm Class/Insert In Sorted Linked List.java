648ac1e58af66ecc6beee78a5c71598d23b8ebd7
Az <xuchenzhe0211@gmail.com>
2019-07-13 15:38:12 -0700

    Create Insert In Sorted Linked List.java

---
+++ Training Plan - Algorithm Class/Insert In Sorted Linked List.java
@@ -0,0 +1,38 @@
+/*
+Insert a value in a sorted linked list.
+
+Examples
+
+L = null, insert 1, return 1 -> null
+L = 1 -> 3 -> 5 -> null, insert 2, return 1 -> 2 -> 3 -> 5 -> null
+L = 1 -> 3 -> 5 -> null, insert 3, return 1 -> 3 -> 3 -> 5 -> null
+L = 2 -> 3 -> null, insert 1, return 1 -> 2 -> 3 -> null
+*/
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode insert(ListNode head, int value) {
+    ListNode newNode = new ListNode(value);
+    if (head == null) return newNode;
+    if (head.value >= value) {
+      newNode.next = head;
+      return newNode;
+    }
+    ListNode cur = head;
+    while (cur.next != null && cur.next.value < value) {
+        cur = cur.next;
+    }
+    newNode.next = cur.next;
+    cur.next = newNode;
+    return head;
+  }
+}
