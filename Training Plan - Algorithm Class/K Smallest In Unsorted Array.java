4535bab25cd02a846f71b7986699e0bfbd257d08
Az <xuchenzhe0211@gmail.com>
2019-07-15 21:48:04 -0700

    Create K Smallest In Unsorted Array.java

---
+++ Training Plan - Algorithm Class/K Smallest In Unsorted Array.java
@@ -0,0 +1,34 @@
+/*
+Find the K smallest numbers in an unsorted integer array A. The returned numbers should be in ascending order.
+
+Assumptions
+
+A is not null
+K is >= 0 and smaller than or equal to size of A
+Return
+
+an array with size K containing the K smallest numbers in ascending order
+Examples
+
+A = {3, 4, 1, 2, 5}, K = 3, the 3 smallest numbers are {1, 2, 3}
+*/
+
+public class Solution {
+  public int[] kSmallest(int[] array, int k) {
+    int[] result = new int[k];
+    if (array == null || array.length == 0 || k == 0) return result;
+    PriorityQueue<Integer> pq = new PriorityQueue<>(k, Collections.reverseOrder());
+    for (int i = 0; i < array.length; i++) {
+      if (i < k) {
+        pq.offer(array[i]);
+      } else if (array[i] < pq.peek()){
+        pq.poll();
+        pq.offer(array[i]);
+      }
+    }
+    for (int i = k - 1; i >= 0; i--) {
+      result[i] = pq.poll();
+    }
+    return result;
+  }
+}
