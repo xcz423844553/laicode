372d29736a74534d54a9715b706df95f9a77229d
Az <xuchenzhe0211@gmail.com>
2019-07-23 21:57:22 -0700

    Create Lowest Common Ancestor I.java

---
+++ Training Plan - Algorithm Class/Lowest Common Ancestor I.java
@@ -0,0 +1,47 @@
+/*
+Given two nodes in a binary tree, find their lowest common ancestor.
+
+Assumptions
+
+There is no parent pointer for the nodes in the binary tree
+
+The given two nodes are guaranteed to be in the binary tree
+
+Examples
+
+        5
+      /   \
+     9     12
+   /  \      \
+  2    3      14
+
+The lowest common ancestor of 2 and 14 is 5
+
+The lowest common ancestor of 2 and 9 is 9
+*/
+
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root,
+      TreeNode one, TreeNode two) {
+    if (root == null) return null;
+    if (root.key == one.key || root.key == two.key) return root;
+    TreeNode left = lowestCommonAncestor(root.left, one, two);
+    TreeNode right = lowestCommonAncestor(root.right, one, two);
+    if (left != null && right != null) {
+      return root; 
+    } else if (left != null || right != null) {
+      return left == null ? right : left;
+    }
+    return null;
+  }
+}
