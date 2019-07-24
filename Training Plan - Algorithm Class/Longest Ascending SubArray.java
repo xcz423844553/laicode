8630fc402186aac92c9cefccc6e1890f922af130
Az <xuchenzhe0211@gmail.com>
2019-07-23 22:14:11 -0700

    Create Longest Ascending SubArray.java

---
+++ Training Plan - Algorithm Class/Longest Ascending SubArray.java
@@ -0,0 +1,31 @@
+/*
+Given an unsorted array, find the length of the longest subarray in which the numbers are in ascending order.
+
+Assumptions
+
+The given array is not null
+
+Examples
+
+{7, 2, 3, 1, 5, 8, 9, 6}, longest ascending subarray is {1, 5, 8, 9}, length is 4.
+
+{1, 2, 3, 3, 4, 4, 5}, longest ascending subarray is {1, 2, 3}, length is 3.
+*/
+
+public class Solution {
+  public int longest(int[] array) {
+    if (array == null || array.length == 0) return 0;
+    int[] m = new int[array.length];
+    int max = 1;
+    m[0] = 1;
+    for (int i = 1; i < m.length; i++) {
+      if (array[i] <= array[i - 1]) {
+        m[i] = 1;
+      } else {
+        m[i] = m[i - 1] + 1;        
+      }
+      max = Math.max(max, m[i]);
+    }
+    return max;
+  }
+}
