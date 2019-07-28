394dc8e41472dea5e96b159f9c90fef671e157b6
Az <xuchenzhe0211@gmail.com>
2019-07-28 09:21:44 -0700

    Create Generalized Reservoir Sampling.java

---
+++ Training Plan - Algorithm Class/Generalized Reservoir Sampling.java
@@ -0,0 +1,37 @@
+/*
+Consider an unlimited flow of data elements. How do you sample k element from this flow, such that at any point during the processing of the flow, you can return a random set of k elements from the n elements read so far. 
+
+Assumptions
+
+k >= 1
+You will implement two methods for a sampling class:
+
+read(int value) - read one number from the flow
+sample() - return at any time the k samples as a list, return the list of all values read when the number of values read so fas <= k.
+You may need to add more fields for the class.
+*/
+
+public class Solution {
+  private final int k;
+  private List<Integer> samples;
+  private int count;
+  public Solution(int k) {
+    this.count = 0;
+    this.samples = new ArrayList<>();
+    this.k = k;
+  }
+  
+  public void read(int value) {
+    count++;
+    if (count <= k) {
+      samples.add(value);
+    } else {
+      int r = new Random().nextInt(count);
+      if (r < k) samples.set(r, value);
+    }
+  }
+  
+  public List<Integer> sample() {
+    return samples;
+  }
+}
