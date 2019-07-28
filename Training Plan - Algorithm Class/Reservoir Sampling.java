c4c0e5be2c0aa2410a00bb95d3c723e034df2f01
Az <xuchenzhe0211@gmail.com>
2019-07-28 09:14:07 -0700

    Create Reservoir Sampling.java

---
+++ Training Plan - Algorithm Class/Reservoir Sampling.java
@@ -0,0 +1,29 @@
+/*
+Consider an unlimited flow of data elements. How do you sample one element from this flow, such that at any point during the processing of the flow, you can return a random element from the n elements read so far.
+
+You will implement two methods for a sampling class:
+
+read(int value) - read one number from the flow
+sample() - return at any time the sample, if n values have been read, the probability of returning any one of the n values is 1/n, return null(Java)/INT_MIN(C++) if there is no value read so far
+You may need to add more fields for the class.
+*/
+
+public class Solution {
+  int count;
+  int sample;
+  public Solution() {
+    count = 0; 
+    sample = 0;
+  }
+  
+  public void read(int value) {
+    count++;
+    int r = new Random().nextInt(count + 1);
+    if (r == 0) sample = value;
+  }
+  
+  public Integer sample() {
+    if (count == 0) return null;
+    return sample;
+  }
+}
