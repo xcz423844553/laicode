e84c59d6fc457679553236c6f63d66a2a6023f1f
Az <xuchenzhe0211@gmail.com>
2019-08-20 21:18:05 -0700

    Update Largest Rectangle Of 1s.java

--- Training Plan - Algorithm Class/Largest Rectangle Of 1s.java
+++ Training Plan - Algorithm Class/Largest Rectangle Of 1s.java
@@ -18,6 +18,7 @@ Examples
 the largest rectangle of 1s has area of 2 * 3 = 6
 */
 
+// Solution 1
 public class Solution {
   public int largest(int[][] matrix) {
     int[][] len = new int[matrix.length][matrix[0].length];
@@ -45,5 +46,33 @@ public class Solution {
       }
     }
     return area;
+  }
+}
+
+// Solution 2
+public class Solution {
+  public int largest(int[][] matrix) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    int max = 0;
+    for (int i = 0; i < m; i++) {
+      if (i > 0) {
+        for (int j = 0; j < n; j++) {
+          matrix[i][j] = matrix[i][j] == 0 ? 0 : matrix[i - 1][j] + 1;
+        }
+      }
+      Deque<Integer> stack = new LinkedList<>();
+      for (int j = 0; j <= n; j++) {
+        int newVal = j == n ? 0 : matrix[i][j];
+        while (!stack.isEmpty() && matrix[i][stack.peekFirst()] > newVal) {
+          int idx = stack.pollFirst();
+          int h = matrix[i][idx];
+          int w = stack.isEmpty() ? j : j - (stack.peekFirst() + 1);
+          max = Math.max(max, h * w);
+        }
+        stack.offerFirst(j);
+      }
+    }
+    return max;
   }
 }
