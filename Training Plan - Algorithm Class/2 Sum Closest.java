2ed1d1e235cf53abecbe1ce5b94ba2488bf2cd7c
Az <xuchenzhe0211@gmail.com>
2019-10-27 11:17:57 -0700

    Create 2 Sum Closest.java

---
+++ Training Plan - Algorithm Class/2 Sum Closest.java
@@ -0,0 +1,35 @@
+/*
+Find the pair of elements in a given array that sum to a value that is closest to the given target number. Return the values of the two numbers.
+
+Assumptions
+
+The given array is not null and has length of at least 2
+
+Examples
+
+A = {1, 4, 7, 13}, target = 7, closest pair is 1 + 7 = 8, return [1, 7].
+*/
+
+public class Solution {
+  public List<Integer> closest(int[] array, int target) {
+    Integer[] res = new Integer[2];
+    Arrays.sort(array);
+    int left = 0, right = array.length - 1;
+    int diff = Integer.MAX_VALUE;
+    while (left < right) {
+      int sum = array[left] + array[right];
+      if (Math.abs(sum - target) < diff) {
+        res[0] = array[left];
+        res[1] = array[right];
+      }
+      if (sum == target) {
+        break;
+      } else if (sum < target) {
+        left++;
+      } else {
+        right++;
+      }
+    }
+    return Arrays.asList(res);
+  }
+}
